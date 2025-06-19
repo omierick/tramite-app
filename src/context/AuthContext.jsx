600bdaebbc2217d05fcea06239ab9a27acef54b3
Erick Olguin <erick.olguin.barrera@gmail.com>
2025-06-19 12:55:32 -0600

    Fix local storage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,23 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    // Intentar cargar el usuario desde localStorage
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
-  // Recibe todo el usuario {nombre, rol, correo}
+  // Guardar el usuario en localStorage cuando cambia
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
   const login = (userData) => setUser(userData);
   const logout = () => setUser(null);
 
